25c2e9e301119fccbb356ed587f2775e9ab8f8f7
zhurand <a_zhuravlev1@mail.ru>
2024-12-08 13:32:35 +0300

    Добавил javadoc для TaskList

--- src/TaskList.java
+++ src/TaskList.java
@@ -1,22 +1,48 @@
 import java.util.HashMap;
-
+/**
+ * Класс списка с задачами со свойствами <b>taskList</b>, <b>status</b>, <b>taskInDict</b>, <b>statusInDict</b>,
+ * <b>nameTaskList</b> и <b>taskID</b>.
+ * @autor zhurand
+ * @version 1.0
+ */
 public class TaskList{
+    /** Поле словарь всех списков задач */
     private HashMap<String, HashMap> taskList = new HashMap<>();
+    /** Поле словарь статусов задач во всех списках */
     private HashMap<String, HashMap> status = new HashMap<>();
-    private HashMap<Integer, String> taskInDict = new HashMap<>();;
+    /** Поле задача в словаре */
+    private HashMap<Integer, String> taskInDict = new HashMap<>();
+    /** Поле статус в словаре */
     private HashMap<Integer, String> statusInDict = new HashMap<>();
+    /** Поле имя списка задач */
     private String nameTaskList;
+    /** Поле идентификатор задачи */
     private int taskID;
+    /** Создание нового списка задач
+     * @param nameTaskList имя списка задач
+     * */
     public TaskList(String nameTaskList) {
         this.nameTaskList = nameTaskList;
         taskList.put(this.nameTaskList, taskInDict);
     }
+    /**
+     * Функция получения значения поля {@link TaskList#nameTaskList}
+     * @return возвращает название списка задач
+     */
     public String getNameTaskList() {
         return nameTaskList;
     }
+    /**
+     * Процедура определения названия списка задач
+     * @param nameTaskList название списка задач
+     */
     public void setNameTaskList(String nameTaskList) {
         this.nameTaskList = nameTaskList;
     }
+    /**
+     * Процедура добавления задачи в словарь со всеми списками задач
+     * @param task задача
+     */
     public void addTask(Task task) {
         if (taskList.isEmpty() || taskList.get(task.getNameTaskList()) == null) {
             taskID = 1;
@@ -39,6 +65,9 @@ public class TaskList{
             status.put(nameTaskList, (HashMap) statusInDict.clone());
         }
     }
+    /**
+     * Процедура вывода всех списков задач в консоль
+     */
     public void printTaskList() {
         for (String key : taskList.keySet()) {
             System.out.print("\n### " + key + " ###\n");
@@ -54,6 +83,11 @@ public class TaskList{
             System.out.print("----------\n");
         }
     }
+    /**
+     * Функция определения наличия списка задач в словаре taskList
+     * @param nameTaskList имя списка задач
+     * @return возвращает true если список задач есть в taskList, false если его нет
+     */
     public Boolean existsTaskList(String nameTaskList) {
         boolean exists = false;
         for (String key : taskList.keySet()) {
@@ -64,24 +98,53 @@ public class TaskList{
         }
         return exists;
     }
+    /** Функция получения описания задачи
+     * @param nameTaskList имя списка задач
+     * @param taskID идентификатор задачи
+     * @return возвращает описание задачи */
     public String getTaskDescription(String nameTaskList, int taskID) {
         return (String) taskList.get(nameTaskList).get(taskID);
     }
+    /**
+     * Процедура редактирования описания задачи
+     * @param nameTaskList название списка задач
+     * @param taskID идентификатор задачи
+     * @param newTaskDescription новое описание задачи
+     */
     public void editTaskDescription(String nameTaskList, int taskID, String newTaskDescription) {
         HashMap task = taskList.get(nameTaskList);
         task.put(taskID, newTaskDescription);
     }
+    /**
+     * Процедура редактирования статуса задачи
+     * @param nameTaskList название списка задач
+     * @param taskID идентификатор задачи
+     * @param newStatus новый статус задачи
+     */
     public void editStatusTask(String nameTaskList, int taskID, String newStatus) {
         HashMap task = status.get(nameTaskList);
         task.put(taskID, newStatus);
     }
+    /**
+     * Процедура удаления списка статусов для списка задач
+     * @param nameTaskList название списка задач
+     */
     public void deleteStatusList(String nameTaskList) {
         status.remove(nameTaskList);
     }
+    /**
+     * Процедура удаления списка задач
+     * @param nameTaskList название списка задач
+     */
     public void deleteTaskList(String nameTaskList) {
         taskList.remove(nameTaskList);
         deleteStatusList(nameTaskList);
     }
+    /**
+     * Процедура удаления задачи из списка задач
+     * @param nameTaskList название списка задач
+     * @param taskID идентификатор задачи
+     */
     public void deleteTask(String nameTaskList, int taskID) {
         int idTaskForMapTasks = taskID;
         int idTaskForMapStatus = taskID;
@@ -104,9 +167,14 @@ public class TaskList{
         mapStatus.remove(mapStatus.size());
         mapStatusCopy.clear();
     }
+    /** Функция получения размера списка задач
+     * @param nameTaskList имя списка задач
+     * @return возвращает размер списка задач */
     public int sizeTaskList(String nameTaskList) {
         return taskList.get(nameTaskList).size();
     }
+    /** Функция получения размера словара со списками задач
+     * @return возвращает размер словаря со списками задач */
     public int size() {
         return taskList.size();
     }
